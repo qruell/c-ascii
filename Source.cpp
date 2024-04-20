7e3dc8950820e5bdd25b1b3fefc4bd67212d2483
qruell <illya.goncharuk.angelok@gmail.com>
2024-04-20 16:31:43 +0300

    ex1

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name       : \"The war and the peace\"" << endl;
+    cout << "Avtor      :   L.N. Tolstoj" << endl;
+    cout << "Izdatelstvo:  Piter" << endl;
+    cout << "Pages      : 500" << endl;
+    return 0;
+}
