139dd9c45a1267dc5857a1ecb08502469f124e6d
qruell <illya.goncharuk.angelok@gmail.com>
2024-04-20 17:41:28 +0300

    ex3

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
 
 int main() {
-	std::cout << "\n"
-		<< (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 "/n"
-		<< (char)186 << "/n";
+    std::cout << "\n" << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205  << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << "\n";
+    std::cout << (char)186 << "         Vremena goda          " << (char)186 << "\n";
+    std::cout << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)185 << "\n";
+    std::cout << (char)186 << " Zima\t" << (char)186 << " Vesna\t" << (char)186 << " Leto\t" << (char)186 << " Osen\t" << (char)186 << "\n";
+    std::cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205  << (char)188 << "\n";
 
+    return 0;
 }
