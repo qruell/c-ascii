906e2c7c297b7098ca123dce888bc965f99c6c73
qruell <illya.goncharuk.angelok@gmail.com>
2024-04-20 16:58:54 +0300

    ex3

---
+++ Source.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+int main() {
+	std::cout << "\n"
+		<< (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 "/n"
+		<< (char)186 << "/n";
+
+}
